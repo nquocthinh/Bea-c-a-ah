896584e7fb0b02a0ed3820a66561c2556463de7a
nquocthinh <90739775+nquocthinh@users.noreply.github.com>
2021-09-15 15:48:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Nguyễn Mai Đình!',
+    introDesc: `Chào mừng Bae đã đén với tim anh <3 
+    Thủ đô của sự chân thành và tình yêu bao la của anh.`,
+    btnIntro: 'Nhấn vào đây để vào tim anh --->',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    desc: 'Bae có thương anh hông :(( ',
+    btnYes: 'Yêu anhh <33',
+    btnNo: 'Không, Anh à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3 ?',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em không biết nhưng mà yêu anh nhắm ',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Uống trà sữa không tí anh mang qua ??.',
+    btnAccept: 'Dạ uống <3',
+    messLink: 'https://www.facebook.com/nquocthinh06maiyeuem/' //
 }
